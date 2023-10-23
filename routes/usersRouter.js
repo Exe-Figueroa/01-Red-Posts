1c0507d19e50061cbf0f9b319d0e685ff878f759
Exe-Figueroa <facundofigueroa789@gmail.com>
2023-10-23 14:39:59 -0300

    Se eliminan las rutas para solicitar usuarios

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -29,30 +29,30 @@ router.get('/:id',
     }
   });
 
-router.get('/email/:email',
-  async (req, res) => {
-    try {
-      const { email } = req.params;
-      const user = await service.findByEmail(email + '@gmail.com');
-      res.json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+// router.get('/email/:email',
+//   async (req, res) => {
+//     try {
+//       const { email } = req.params;
+//       const user = await service.findByEmail(email + '@gmail.com');
+//       res.json(user);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   });
 
-router.get('/username/:username',
-  async (req, res) => {
-    try {
-      const { username } = req.params;
-      console.log({username});
-      const user = await service.findByUsername(username);
-      res.json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+// router.get('/username/:username',
+//   async (req, res) => {
+//     try {
+//       const { username } = req.params;
+//       console.log({username});
+//       const user = await service.findByUsername(username);
+//       res.json(user);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   });
 
 router.post('/',
   validatorHandler(createUserSchema, 'body'),
